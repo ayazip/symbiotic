9a5637104215ba69e01834bb914310dd2a05d1dc
Paulína Ayazi <paulina.ayazi@gmail.com>
2023-11-20 19:43:42 +0100

    fix abs.c

--- lib/libc/witch-klee/abs.c
+++ lib/libc/witch-klee/abs.c
@@ -6,8 +6,6 @@ int abs(int x) {
     if (x == INT_MIN)
         __VERIFIER_error_overflow();
     if (x >= 0)
-    if (x >= 0)
-        return x;
         return x;
     return -x;
-    return -x;
+}
