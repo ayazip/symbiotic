b6f0db174142abe43ddf23cadea04e1871380938
Paulína Ayazi <paulina.ayazi@gmail.com>
2023-11-20 19:30:24 +0100

    Add/Fix function models

--- lib/libc/witch-klee/abs.c
+++ lib/libc/witch-klee/abs.c
@@ -1,5 +1,13 @@
-long abs(long x) {
+#include <limits.h>
+extern void __VERIFIER_error_overflow();
+
+
+int abs(int x) {
+    if (x == INT_MIN)
+        __VERIFIER_error_overflow();
+    if (x >= 0)
     if (x >= 0)
         return x;
+        return x;
     return -x;
-}
+    return -x;
